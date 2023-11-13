7b5a7f0fd371dcc58c90d867ecc93f241528a43d
Raquel <132924710+raquellissat@users.noreply.github.com>
2023-11-13 11:34:51 -0300

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -2,57 +2,57 @@
 let questions = [
   {
     numb: 1,
-    question: "Quem foi a única pessoa na história a receber o Prêmio Nobel em áreas científicas diferentes?",
-    answer: "Marie Curiee",
+    question: "De quem é a famosa frase 'penso, logo existo' ?",
+    answer: "Descartes",
     options: [
-      "Albert Einstein",
-      "Marie Curiee",
-      "Stephen Hawking",
-      "Linus Pauling"
+      "Platão",
+      "Galileu Galilei",
+      "Descartes",
+      "Sócrates"
     ]
   },
   {
     numb: 2,
-    question: "Qual o país mais novo do mundo?",
-    answer: "Sudão do Sul",
+    question: "Quantas casas decimais tem o número pi??",
+    answer: "Infinitas",
     options: [
-      "Timor Lestet",
-      "Montenegro",
-      "Sudão do Sul",
-      "Kosovo"
+      "1",
+      "3",
+      "Infinitas",
+      "5"
     ]
   },
   {
     numb: 3,
-    question: "Qual o nome do evento astronômico responsável pelo dia mais longo do ano?",
-    answer: "Solstício de verão",
+    question: "Qual o número mínimo de jogadores em cada time numa partida de futebol??",
+    answer: "7",
     options: [
-      "Equinócio de outono",
-      "Solstício de inverno",
-      "Solstício de verão",
-      "Solstício de outono"
+      "5",
+      "7",
+      "10",
+      "14"
     ]
   },
   {
     numb: 4,
-    question: "Quantos pares de costelas um ser humano, normalmente, possui?",
-    answer: "12",
+    question: "Quem pintou "Guernica"?",
+    answer: "Pablo Picasso",
     options: [
-      "12",
-      "11",
-      "14",
-      "13"
+      "Pablo Picasso",
+      "Paul Cézanne",
+      "Tarsila do Amaral",
+      "Tarsila do Amaralan "
     ]
   },
   {
     numb: 5,
-    question: "Quais planetas do sistema solar realizam seu movimento de rotação em sentido horário?",
-    answer: "Vênus e Urano",
+    question: "Quanto tempo a luz do Sol demora para chegar à Terra?",
+    answer: "8",
     options: [
-      "Saturno e Júpiter",
-      "Vênus e Urano",
-      "Terra e Marte",
-      "Mercúrio e Saturno"
+      "4",
+      "9",
+      "5",
+      "8"
     ]
   },
   // you can uncomment the below codes and make duplicate as more as you want to add question
